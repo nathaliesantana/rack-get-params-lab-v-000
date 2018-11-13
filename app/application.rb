c1abaee9fbcdfc52b3974257509af3539f6946f9
Nathalie Santana <nathalie.scruz@gmail.com>
2018-11-13 04:00:17 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -24,7 +24,7 @@ class Application
       end
 
     elsif req.path.match(/add/)
-      item_to_add = req.params ["item"]
+      item_to_add = req.params["item"]
       if @@items.include? item_to_add
         @@cart << item_to_add
       else
